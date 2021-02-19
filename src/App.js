f6685db50ec7ab0d8f2b8dd6f6e7d4cada4f9ce1
Robin Fussell <rfussell17@gmail.com>
2021-02-19 12:59:03 -0800

    refactored and reformatted code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import TodoItem from './components/TodoItem'
-import todosData from './todosData'
-import './App.css';
+import React from "react";
+import TodoItem from "./components/TodoItem";
+import todosData from "./todosData";
+import "./App.css";
 
 function App() {
+  const todoItems = todosData.map((todo) => (
+    <TodoItem key={todo.key} todo={todo} />
+  ));
 
-  const todoItems = todosData.map(todo => <TodoItem key={todo.key} todo={todo}/>)
-
-  return (
-    <div className="App">
-      {todoItems}
-    </div>
-  );
+  return <div className="App">{todoItems}</div>;
 }
 
 export default App;

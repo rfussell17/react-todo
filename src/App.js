4fc0f2b5973342fa476772459f62878fde216fde
Robin Fussell <rfussell17@gmail.com>
2021-02-19 18:23:05 -0800

    toggle checked function added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React, { Component } from "react";
 import TodoItem from "./components/TodoItem";
 import todosData from "./todosData";
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      todos: todosData,
-    };
-  }
-
-  render() {
-    const todoItems = this.state.todos.map((todo) => (
-      <TodoItem key={todo.key} todo={todo} />
-    ));
-
-    return <div className="App">{todoItems}</div>;
-  }
+        todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
 }
 
-export default App;
+    
+handleChange(id) {
+  this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+          if (todo.id === id) {
+              return {
+                  ...todo,
+                  completed: !todo.completed
+              }
+          }
+          return todo
+      })
+      return {
+          todos: updatedTodos
+      }
+  })
+}
+
+render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+    
+    return (
+        <div className="todo-list">
+            {todoItems}
+        </div>
+    )    
+}
+}
+
+export default App
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React from "react"
 
 function TodoItem(props) {
-  return (
-    <div className="todo-item">
-      <input type="checkbox" checked={props.todo.completed} />
-      <p>{props.todo.text}</p>
-    </div>
-  );
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p>{props.item.text}</p>
+        </div>
+    )
 }
 
-export default TodoItem;
+export default TodoItem
